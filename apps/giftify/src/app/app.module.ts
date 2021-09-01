a4a95bf38db03aba3a80fc36428f8c0c7b16ee60
Gieted <gieted@protonmail.com>
2021-09-01 19:32:13 +0200

    Create tags component

--- apps/giftify/src/app/app.module.ts
+++ apps/giftify/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { FiltersComponent } from './filters/filters.component';
 import { InputComponent } from './input/input.component';
 import { RangeSelectorComponent } from './range-selector/range-selector.component';
+import { TagsComponent } from './tags/tags.component';
 
 @NgModule({
-  declarations: [AppComponent, FiltersComponent, InputComponent, RangeSelectorComponent],
+  declarations: [AppComponent, FiltersComponent, InputComponent, RangeSelectorComponent, TagsComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ apps/giftify/src/app/tags/tags.component.stories.ts
@@ -0,0 +1,33 @@
+import { moduleMetadata, Story, Meta } from '@storybook/angular';
+import { TagsComponent } from './tags.component';
+
+export default {
+  title: 'Tags',
+  component: TagsComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [],
+    })
+  ],
+  argTypes: { removeItem: { action: 'removeItem' } },
+} as Meta<TagsComponent>;
+
+const Template: Story<TagsComponent> = (args: TagsComponent) => ({
+  component: TagsComponent,
+  props: args,
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  list: ['sports', 'board games', 'movies', 'books', 'chess']
+}
+
+export const Wrapped = Template.bind({});
+Wrapped.args = {
+  list: ['sports', 'board games', 'movies', 'books', 'chess']
+}
+Wrapped.parameters = {
+  viewport: {
+    defaultViewport: 'mobile1'
+  },
+}

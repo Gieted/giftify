163f625b82713515c87db4bfa3fa038742195f9c
Gieted <gieted@protonmail.com>
2021-09-02 19:43:00 +0200

    Fix tests

--- apps/giftify/src/app/filters/filters.component.spec.ts
+++ apps/giftify/src/app/filters/filters.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FiltersComponent } from './filters.component';
+import { TagsComponent } from '../tags/tags.component';
+import { RangeSelectorComponent } from '../range-selector/range-selector.component';
+import { InputComponent } from '../input/input.component';
 
 describe('FiltersComponent', () => {
   let component: FiltersComponent;
@@ -8,7 +11,7 @@ describe('FiltersComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [FiltersComponent],
+      declarations: [FiltersComponent, TagsComponent, RangeSelectorComponent, InputComponent],
     }).compileComponents();
   });
 

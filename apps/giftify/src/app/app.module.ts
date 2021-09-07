e3fbe2a1ba6592fb173db98f5248fed6fc142f65
Tomasz1234 <37366587+Tomasz1234@users.noreply.github.com>
2021-09-07 11:14:40 +0200

    Gift list component (#2)

--- apps/giftify/src/app/app.module.ts
+++ apps/giftify/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { ListItemComponent } from './list/list-item/list-item.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ListComponent, ListItemComponent],
+  imports: [BrowserModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
